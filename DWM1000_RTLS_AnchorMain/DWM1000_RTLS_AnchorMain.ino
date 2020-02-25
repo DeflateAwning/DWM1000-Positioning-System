3228c439010866191d6ebfb506160dd0bd4474f7
ThePiGuy <ThePiGuy@users.noreply.github.com>
2020-02-25 13:37:23 -0700

    Added hard coded wifi connection to Wifi Hotspot, added basic RequestBin testing

--- DWM1000_RTLS_AnchorMain/DWM1000_RTLS_AnchorMain.ino
+++ DWM1000_RTLS_AnchorMain/DWM1000_RTLS_AnchorMain.ino
@@ -12,14 +12,36 @@
  * This is an example master anchor in a RTLS using two way ranging ISO/IEC 24730-62_2013 messages
  */
 
+// ESP8266 Libraries (Hard Coded)
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+ 
+// ESP8266 Libraries (Using AutoConnect)
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+typedef ESP8266WebServer WEBServer;
+#include <FS.h>
+#include <AutoConnect.h>
+/* For ESP32 Architecture (?)
+#include <WiFi.h>
+#include <WebServer.h>
+#include <SPIFFS.h>
+ */
+
+ 
+// DWM1000 Libraries
 #include <DW1000Ng.hpp>
 #include <DW1000NgUtils.hpp>
 #include <DW1000NgRanging.hpp>
 #include <DW1000NgRTLS.hpp>
 
+// Wifi Configuration Options
+const char* ssid = "UWBLS Net"; // Will's Settings
+const char* password = "123456789"; // Will's Settings
+
 typedef struct Position {
-    double x;
-    double y;
+	double x;
+	double y;
 } Position;
 
 // connection pins
@@ -49,125 +71,188 @@ uint16_t next_anchor = 2;
 byte anchor_c[] = {0x03, 0x00};
 
 device_configuration_t DEFAULT_CONFIG = {
-    false,
-    true,
-    true,
-    true,
-    false,
-    SFDMode::STANDARD_SFD,
-    Channel::CHANNEL_5,
-    DataRate::RATE_850KBPS,
-    PulseFrequency::FREQ_16MHZ,
-    PreambleLength::LEN_256,
-    PreambleCode::CODE_3
+	false,
+	true,
+	true,
+	true,
+	false,
+	SFDMode::STANDARD_SFD,
+	Channel::CHANNEL_5,
+	DataRate::RATE_850KBPS,
+	PulseFrequency::FREQ_16MHZ,
+	PreambleLength::LEN_256,
+	PreambleCode::CODE_3
 };
 
 frame_filtering_configuration_t ANCHOR_FRAME_FILTER_CONFIG = {
-    false,
-    false,
-    true,
-    false,
-    false,
-    false,
-    false,
-    true /* This allows blink frames */
+	false,
+	false,
+	true,
+	false,
+	false,
+	false,
+	false,
+	true /* This allows blink frames */
 };
 
 void setup() {
-    // DEBUG monitoring
-    Serial.begin(115200);
-    Serial.println(F("### DW1000Ng-arduino-ranging-anchorMain ###"));
-    // initialize the driver
-    #if defined(ESP8266)
-    DW1000Ng::initializeNoInterrupt(PIN_SS);
-    #else
-    DW1000Ng::initializeNoInterrupt(PIN_SS, PIN_RST);
-    #endif
-    Serial.println(F("DW1000Ng initialized ..."));
-    // general configuration
-    DW1000Ng::applyConfiguration(DEFAULT_CONFIG);
-    DW1000Ng::enableFrameFiltering(ANCHOR_FRAME_FILTER_CONFIG);
-    
-    DW1000Ng::setEUI("AA:BB:CC:DD:EE:FF:00:01");
+	// Init Debug
+	Serial.begin(115200);
+	Serial.println(F("### DW1000Ng-arduino-ranging-anchorMain ###"));
 
-    DW1000Ng::setPreambleDetectionTimeout(64);
-    DW1000Ng::setSfdDetectionTimeout(273);
-    DW1000Ng::setReceiveFrameWaitTimeoutPeriod(5000);
-
-    DW1000Ng::setNetworkId(RTLS_APP_ID);
-    DW1000Ng::setDeviceAddress(1);
+	// Setup the Wifi Connection
+	setupWifi();
 	
-    DW1000Ng::setAntennaDelay(16436);
-    
-    Serial.println(F("Committed configuration ..."));
-    // DEBUG chip info and registers pretty printed
-    char msg[128];
-    DW1000Ng::getPrintableDeviceIdentifier(msg);
-    Serial.print("Device ID: "); Serial.println(msg);
-    DW1000Ng::getPrintableExtendedUniqueIdentifier(msg);
-    Serial.print("Unique ID: "); Serial.println(msg);
-    DW1000Ng::getPrintableNetworkIdAndShortAddress(msg);
-    Serial.print("Network ID & Device Address: "); Serial.println(msg);
-    DW1000Ng::getPrintableDeviceMode(msg);
-    Serial.print("Device mode: "); Serial.println(msg);    
+	// Setup the DWM1000
+	//setupDWM();
+
+}
+
+void setupWifi() {
+	WiFi.begin(ssid, password);
+
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(1000);
+		Serial.println("Connecting...");
+	}
+
+	Serial.print("\nConnected, IP Address: ");
+	Serial.println(WiFi.localIP());
+}
+
+void setupDWM() {
+  
+	// initialize the driver
+	#if defined(ESP8266)
+	DW1000Ng::initializeNoInterrupt(PIN_SS);
+	#else
+	DW1000Ng::initializeNoInterrupt(PIN_SS, PIN_RST);
+	#endif
+	Serial.println(F("DW1000Ng initialized ..."));
+	// general configuration
+	DW1000Ng::applyConfiguration(DEFAULT_CONFIG);
+	DW1000Ng::enableFrameFiltering(ANCHOR_FRAME_FILTER_CONFIG);
+	
+	DW1000Ng::setEUI("AA:BB:CC:DD:EE:FF:00:01");
+
+	DW1000Ng::setPreambleDetectionTimeout(64);
+	DW1000Ng::setSfdDetectionTimeout(273);
+	DW1000Ng::setReceiveFrameWaitTimeoutPeriod(5000);
+
+	DW1000Ng::setNetworkId(RTLS_APP_ID);
+	DW1000Ng::setDeviceAddress(1);
+  
+	DW1000Ng::setAntennaDelay(16436);
+	
+	Serial.println(F("Committed configuration ..."));
+	// DEBUG chip info and registers pretty printed
+	char msg[128];
+	DW1000Ng::getPrintableDeviceIdentifier(msg);
+	Serial.print("Device ID: "); Serial.println(msg);
+	DW1000Ng::getPrintableExtendedUniqueIdentifier(msg);
+	Serial.print("Unique ID: "); Serial.println(msg);
+	DW1000Ng::getPrintableNetworkIdAndShortAddress(msg);
+	Serial.print("Network ID & Device Address: "); Serial.println(msg);
+	DW1000Ng::getPrintableDeviceMode(msg);
+	Serial.print("Device mode: "); Serial.println(msg);    
 }
 
 /* using https://math.stackexchange.com/questions/884807/find-x-location-using-3-known-x-y-location-using-trilateration */
 void calculatePosition(double &x, double &y) {
 
-    /* This gives for granted that the z plane is the same for anchor and tags */
-    double A = ( (-2*position_self.x) + (2*position_B.x) );
-    double B = ( (-2*position_self.y) + (2*position_B.y) );
-    double C = (range_self*range_self) - (range_B*range_B) - (position_self.x*position_self.x) + (position_B.x*position_B.x) - (position_self.y*position_self.y) + (position_B.y*position_B.y);
-    double D = ( (-2*position_B.x) + (2*position_C.x) );
-    double E = ( (-2*position_B.y) + (2*position_C.y) );
-    double F = (range_B*range_B) - (range_C*range_C) - (position_B.x*position_B.x) + (position_C.x*position_C.x) - (position_B.y*position_B.y) + (position_C.y*position_C.y);
+	/* This gives for granted that the z plane is the same for anchor and tags */
+	double A = ( (-2*position_self.x) + (2*position_B.x) );
+	double B = ( (-2*position_self.y) + (2*position_B.y) );
+	double C = (range_self*range_self) - (range_B*range_B) - (position_self.x*position_self.x) + (position_B.x*position_B.x) - (position_self.y*position_self.y) + (position_B.y*position_B.y);
+	double D = ( (-2*position_B.x) + (2*position_C.x) );
+	double E = ( (-2*position_B.y) + (2*position_C.y) );
+	double F = (range_B*range_B) - (range_C*range_C) - (position_B.x*position_B.x) + (position_C.x*position_C.x) - (position_B.y*position_B.y) + (position_C.y*position_C.y);
 
-    x = (C*E-F*B) / (E*A-B*D);
-    y = (C*D-A*F) / (B*D-A*E);
+	x = (C*E-F*B) / (E*A-B*D);
+	y = (C*D-A*F) / (B*D-A*E);
 }
 
+// Project's SUPER FUCKING LOOP
 void loop() {
-    if(DW1000NgRTLS::receiveFrame()){
-        size_t recv_len = DW1000Ng::getReceivedDataLength();
-        byte recv_data[recv_len];
-        DW1000Ng::getReceivedData(recv_data, recv_len);
+	// Run the DWM1000 Loop
+	//loopDWM();
+
+	// Run the Test Wifi System
+	testWifiRequest();
+
+	//delay(10000);
+	
+}
+
+void testWifiRequest() {
+	Serial.println("Beginning Request Send");
+
+	// Check if Connected
+	if (WiFi.status() != WL_CONNECTED) {
+		Serial.println("Not connected to WiFi.");
+		return;
+	}
+
+	// Make the http client, send request
+	HTTPClient http;
+	http.begin("http://enqb0w8a2ni1j.x.pipedream.net"); //  if this fails, try http instead of https
+	int httpCode = http.GET();
+
+	// Check the response
+	if (httpCode > 0) {
+		String payload = http.getString();
+		Serial.println(payload);
+		Serial.println("\n");
+	}
+	else {
+		Serial.print("Got httpCode!=0, httpCode=");
+		Serial.println(httpCode);
+	}
+	http.end();
 
 
-        if(recv_data[0] == BLINK) {
-            DW1000NgRTLS::transmitRangingInitiation(&recv_data[2], tag_shortAddress);
-            DW1000NgRTLS::waitForTransmission();
+}
 
-            RangeAcceptResult result = DW1000NgRTLS::anchorRangeAccept(NextActivity::RANGING_CONFIRM, next_anchor);
-            if(!result.success) return;
-            range_self = result.range;
+void loopDWM() {
+	// Literal Duplication of F-Army code
+	if(DW1000NgRTLS::receiveFrame()){
+		size_t recv_len = DW1000Ng::getReceivedDataLength();
+		byte recv_data[recv_len];
+		DW1000Ng::getReceivedData(recv_data, recv_len);
 
-            String rangeString = "Range: "; rangeString += range_self; rangeString += " m";
-            rangeString += "\t RX power: "; rangeString += DW1000Ng::getReceivePower(); rangeString += " dBm";
-            Serial.println(rangeString);
 
-        } else if(recv_data[9] == 0x60) {
-            double range = static_cast<double>(DW1000NgUtils::bytesAsValue(&recv_data[10],2) / 1000.0);
-            String rangeReportString = "Range from: "; rangeReportString += recv_data[7];
-            rangeReportString += " = "; rangeReportString += range;
-            Serial.println(rangeReportString);
-            if(received_B == false && recv_data[7] == anchor_b[0] && recv_data[8] == anchor_b[1]) {
-                range_B = range;
-                received_B = true;
-            } else if(received_B == true && recv_data[7] == anchor_c[0] && recv_data[8] == anchor_c[1]){
-                range_C = range;
-                double x,y;
-                calculatePosition(x,y);
-                String positioning = "Found position - x: ";
-                positioning += x; positioning +=" y: ";
-                positioning += y;
-                Serial.println(positioning);
-                received_B = false;
-            } else {
-                received_B = false;
-            }
-        }
-    }
+		if(recv_data[0] == BLINK) {
+			DW1000NgRTLS::transmitRangingInitiation(&recv_data[2], tag_shortAddress);
+			DW1000NgRTLS::waitForTransmission();
 
-    
+			RangeAcceptResult result = DW1000NgRTLS::anchorRangeAccept(NextActivity::RANGING_CONFIRM, next_anchor);
+			if(!result.success) return;
+			range_self = result.range;
+
+			String rangeString = "Range: "; rangeString += range_self; rangeString += " m";
+			rangeString += "\t RX power: "; rangeString += DW1000Ng::getReceivePower(); rangeString += " dBm";
+			Serial.println(rangeString);
+
+		} else if(recv_data[9] == 0x60) {
+			double range = static_cast<double>(DW1000NgUtils::bytesAsValue(&recv_data[10],2) / 1000.0);
+			String rangeReportString = "Range from: "; rangeReportString += recv_data[7];
+			rangeReportString += " = "; rangeReportString += range;
+			Serial.println(rangeReportString);
+			if(received_B == false && recv_data[7] == anchor_b[0] && recv_data[8] == anchor_b[1]) {
+				range_B = range;
+				received_B = true;
+			} else if(received_B == true && recv_data[7] == anchor_c[0] && recv_data[8] == anchor_c[1]){
+				range_C = range;
+				double x,y;
+				calculatePosition(x,y);
+				String positioning = "Found position - x: ";
+				positioning += x; positioning +=" y: ";
+				positioning += y;
+				Serial.println(positioning);
+				received_B = false;
+			} else {
+				received_B = false;
+			}
+		}
+	}
 }
