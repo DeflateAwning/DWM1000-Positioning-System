559694478cd9e29f51f09173afc3eca34c4c0128
Parker Link <ThePiGuy@users.noreply.github.com>
2020-03-21 23:21:35 -0600

    Change testing framework to allow decision between SERIAL or GPIO_OUTPUT testing, add DS gitignore

--- DWM1000_RTLS_TestComponents/DWM1000_RTLS_TestComponents.ino
+++ DWM1000_RTLS_TestComponents/DWM1000_RTLS_TestComponents.ino
@@ -1,22 +1,48 @@
+// DWM1000_RTLS_TestComponents.ino
 
+// This program tests all components on the board
+
+
+// Optional: Define Testing
+//#define ENABLE_DWM1000_TESTING 1
+#define ENABLE_PIN_COPY 1
+#define ENABLE_SWITCH_TESTING 1 // always leave on
+//#define ENABLE_SERIAL 1
+
+
+// Currently Working: ENABLE_SWITCH_TESTING+ENABLE_SERIAL
+// Currently Working: ENABLE_SWITCH_TESTING+ENABLE_PIN_COPY for Out1 only
+
+// Disable Wifi
+#include "ESP8266WiFi.h"
+/*
+extern "C" {
+	#include "user_interface.h"
+}
+*/
 
 // DWM1000 Libraries
+#ifdef ENABLE_DWM1000_TESTING
 #include <DW1000Ng.hpp>
 #include <DW1000NgUtils.hpp>
 #include <DW1000NgRanging.hpp>
 #include <DW1000NgRTLS.hpp>
+#endif
 
 // connection pins
 const uint8_t PIN_DWM_SS = 15;
 
-const uint8_t PIN_IN1 = 3; // GPIO3, also RXD, switch
+const uint8_t PIN_IN0 = 0; // GPIO0, FLASH switch
+const uint8_t PIN_IN1 = 3; // GPIO3/RXD, switch
 const uint8_t PIN_IN2 = 10; // GPIO10, also Vibration sensor, switch
 
-const uint8_t PIN_OUT1 = 1; // GPIO1, LED 1
-const uint8_t PIN_OUT2 = 9; // GPIO9, LED 2
+const uint8_t PIN_BUILTIN_LED = 2; // or LED_BUILTIN macro
+const uint8_t PIN_OUT1 = 1; // GPIO1/TXD, LED 1
+const uint8_t PIN_OUT2 = 9; // GPIO9, LED 2 (Requires DIO) - BREAKS EVERYTHING, LEAVE COMMENTED
 
 const uint8_t PIN_ANALOG = A0;
 
+#ifdef ENABLE_DWM1000_TESTING
 device_configuration_t DEFAULT_CONFIG = {
 	false,
 	true,
@@ -30,7 +56,6 @@ device_configuration_t DEFAULT_CONFIG = {
 	PreambleLength::LEN_256,
 	PreambleCode::CODE_3
 };
-
 frame_filtering_configuration_t ANCHOR_FRAME_FILTER_CONFIG = {
 	false,
 	false,
@@ -41,24 +66,44 @@ frame_filtering_configuration_t ANCHOR_FRAME_FILTER_CONFIG = {
 	false,
 	true /* This allows blink frames */
 };
+#endif
 
 void setup() {
-  	// Init Debug
-	Serial.begin(115200);
-	Serial.println(F("\n### UWBLS-DWM1000-RTLS-TestComponents ###"));
+	// Init Debug
+	#ifdef ENABLE_SERIAL
+	//Serial.begin(115200);
+	Serial.begin(115200, SERIAL_8N1, SERIAL_TX_ONLY);
+	Serial.println("\n### UWBLS-DWM1000-RTLS-TestComponents ###");
+	#endif
+
+	// Disable Wifi
+	WiFi.forceSleepBegin();
+	delay(1);
+
+	// Set the Watchdog
+	//ESP.wdtEnable(12000);
 
 	// GPIO input and output setup
-	pinMode(PIN_IN1, INPUT);
-	pinMode(PIN_IN2, INPUT);
+	#ifdef ENABLE_SWITCH_TESTING
+	pinMode(PIN_IN0, INPUT_PULLUP);
+	pinMode(PIN_IN1, INPUT_PULLUP);
+	pinMode(PIN_IN2, INPUT_PULLUP);
+	#endif
+
+	#ifdef ENABLE_PIN_COPY
 	pinMode(PIN_OUT1, OUTPUT);
-	pinMode(PIN_OUT2, OUTPUT);
-	
-	#if defined(DWM1000Testing)
+	//pinMode(PIN_OUT2, OUTPUT);
+	#endif
+	pinMode(PIN_BUILTIN_LED, OUTPUT);
+
+	#ifdef ENABLE_DWM1000_TESTING
 	// Setup the DWM1000
 	setupDWM();
 	#endif
+
 }
 
+#ifdef ENABLE_DWM1000_TESTING
 void setupDWM() {
   
 	// initialize the driver
@@ -92,45 +137,81 @@ void setupDWM() {
 	DW1000Ng::getPrintableDeviceMode(msg);
 	Serial.print("Device mode: "); Serial.println(msg);    
 }
-
+#endif
 
 void loop() {
-	
-	Serial.println("Starting Test");
-	delay(500);
-	Serial.println("LED 1 ON");
-	digitalWrite(PIN_OUT1, HIGH);
-	delay(500);
-	digitalWrite(PIN_OUT1,LOW);
-	
-	Serial.println("LED 2 ON");
-	digitalWrite(PIN_OUT2, HIGH);
-	delay(500);
-	digitalWrite(PIN_OUT2, LOW);
-	
-	Serial.println("Testing Button 1");
-	for(int i = 0; i < 15; i++)
-	{
-		int Button1 = digitalRead(PIN_IN1);
-		if(Button1 == 1)
-			Serial.println("Button 1 PRESSED");
-		else
-			Serial.println("Button 1 NOT PRESSED");
-		delay(250);
-	}
-	
-	Serial.println("Testing Button 2");
-	for(int i = 0; i < 15; i++)
-	{
-		int Button2 = digitalRead(PIN_IN2);
-		if(Button2 == 1)
-			Serial.println("Button 2 PRESSED");
-		else
-			Serial.println("Button 2 NOT PRESSED");
-		delay(250);
-	}
-	
-	int sensorValue = analogRead(PIN_ANALOG);
-	float voltage = (float)sensorValue * 80.0 / 1024.0 / 5.0;
-	
+	// Call Main Sensor/Output Test
+	runSingleTestCycle();
+
+	// Reset the Watchdog (must be done minimum every 3.2s)
+	//feedWatchdog();
 }
+
+long startTime = 0;
+
+void runSingleTestCycle() {
+	#ifdef ENABLE_SERIAL
+	Serial.println("Starting Test Cycle");
+	#endif
+
+	// Do Pin Test, including output testing and input testing
+	doPinTest();
+	
+	doAnalogVoltageTest();
+
+	doSwitchTest();
+}
+
+void doAnalogVoltageTest() {
+	// Read analog values
+	int sensorValue = analogRead(PIN_ANALOG);
+	float voltage = (float)sensorValue * 80.0 / 1024.0 / 5.1;
+	
+	// Print the analog values, if enabled
+	#ifdef ENABLE_SERIAL
+	Serial.print("# Battery Level: "); Serial.print(sensorValue); Serial.print("/1024, or "); Serial.print(voltage); Serial.print(" volts");
+	Serial.println();
+	#endif
+}
+
+void doPinTest() {
+
+	// Always do the built in LED flash
+	#ifdef ENABLE_SERIAL
+	Serial.println("# Built In LED ON/OFF Test");
+	#endif
+
+	digitalWrite(PIN_BUILTIN_LED, HIGH);
+	#ifdef ENABLE_PIN_COPY
+	digitalWrite(PIN_OUT1, HIGH);
+	//digitalWrite(PIN_OUT2, HIGH);
+	#endif
+	delay(1000);
+
+	digitalWrite(PIN_BUILTIN_LED, LOW);
+	#ifdef ENABLE_PIN_COPY
+	digitalWrite(PIN_OUT1, LOW);
+	//digitalWrite(PIN_OUT2, LOW);
+	#endif
+	delay(1000);
+
+}
+
+void doSwitchTest() {
+	startTime = millis();
+	while (millis() - startTime < 5000) {
+		#ifdef ENABLE_PIN_COPY
+		digitalWrite(PIN_BUILTIN_LED, digitalRead(PIN_IN2));
+		digitalWrite(PIN_OUT1, digitalRead(PIN_IN1));
+		#endif
+
+		#ifdef ENABLE_SERIAL
+		Serial.print("Button States (0/1/2): "); Serial.print(digitalRead(PIN_IN0)); Serial.print("/"); Serial.print(digitalRead(PIN_IN1)); Serial.print("/"); Serial.print(digitalRead(PIN_IN2));
+		Serial.println();
+		#endif
+
+		delay(100);
+	}
+}
+
+
--- README.md
+++ README.md
@@ -15,12 +15,12 @@ This section describes the parameters, sent by GET query, to the web server. Eac
 5. ReceivePower
 
 ## Major To Dos
-1. Implement anchor-side timing system, maybe with NTP
+1. ~Implement anchor-side timing system, maybe with NTP~
 2. Anchor numbering system, with either P2P or a configuration or something better
 3. Set all Wifi credentials from main anchor by DWM communication
 4. Remove hardcoding of Wifi credentials
 5. Add division of bands
-6. Consider adding a watchdog
+6. ~Consider adding a watchdog~
 
 ## Next To Do
 * Fix issue with sending query string to server (it's giving httpCode = -1)
@@ -42,3 +42,9 @@ This section describes the parameters, sent by GET query, to the web server. Eac
 	* Switch GET request to JSON POST request (but only one ranging per submission)
 * Add queue design
 	* Multiple rangings per JSON POST request to server
+
+## Board Design Notes
+* IN1/RXD only works when the UART converter is disconnected (`Serial.begin()` only enables Serial TX)
+	* All inputs are active LOW
+* OUT2/GPIO9 is always in use by the device itself (Flash SPI Bus), and cannot be used as a GPIO Output. Oops.
+	* OUT1 is active HIGH, BUILTIN is active LOW
